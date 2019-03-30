ee80d470ba8f6fa8281a6ee2ce881be2c459f0c3
bhmcarr <bhmcarr@gmail.com>
2019-03-30 17:44:28 -0400

    add images and more CSS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  renderCard(i,j,k){
+  renderCard(i,j,k,l){
     return(
       <Card
         day={i}
         high_temp={j}
         low_temp={k}
+        image_link={l}
       />
 
     );
@@ -16,27 +17,13 @@ class App extends Component {
   render(){
     return(
       <div>
-        <div className="card">
-          {this.renderCard("Monday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Tuesday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Wednesday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Thursday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Friday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Saturday", 10, 5)}
-        </div>
-        <div className="card">
-          {this.renderCard("Sunday", 10, 5)}
-        </div>
+          {this.renderCard("Monday", 10, 5, "/img/cloud.png")}
+          {this.renderCard("Tuesday", 10, 5, "/img/sun.png")}
+          {this.renderCard("Wednesday", 10, 5, "/img/sun.png")}
+          {this.renderCard("Thursday", 10, 5, "/img/cloud.png")}
+          {this.renderCard("Friday", 10, 5, "/img/cloud.png")}
+          {this.renderCard("Saturday", 10, 5, "/img/sun.png")}
+          {this.renderCard("Sunday", 10, 5, "/img/sun.png")}
       </div>
     );
   }
@@ -49,16 +36,17 @@ class Card extends Component {
       high_temp:  this.props.high_temp,
       low_temp:   this.props.low_temp,
       day:        this.props.day,
-      image_link: null,
+      image_link: this.props.image_link,
     };
   }
 
   render(){
     return(
-      <div>
-        <p>{this.state.day}</p>
+      <div className="card">
+        <p className="day_text">{this.state.day}</p>
         <p>High: {this.state.high_temp}</p>
         <p>Low: {this.state.low_temp}</p>
+        <img src={this.state.image_link} className="weather_icon"></img>
       </div>
     );
   }
