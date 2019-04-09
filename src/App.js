a13ff44a67e0a7a38fcd68a499294db87438798a
xelti <bhmcarr@gmail.com>
2019-04-09 10:36:13 -0400

    add city change form and code to update data with new city. fix clock display bug.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends Component {
     super(props);
     var date = new Date();
     var forecastDays = [date.getDay(), date.getDay()+1, date.getDay()+2, date.getDay()+3, date.getDay()+4];
+
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleSubmit     = this.handleSubmit.bind(this);
+
     this.state = {
       hours:   date.getHours(),
       minutes: date.getMinutes(),
@@ -48,9 +52,31 @@ class App extends Component {
     );
   }
 
+  handleCityChange(event){
+    this.setState({city: event.target.value});
+  }
+
+  handleSubmit(event){
+    //alert('City changed to: ' + this.state.city);
+    const appId="b6fd93a747972ebea2d0b6ae6dc84c8b";
+    const URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + this.state.city + "&cnt=7&units=imperial&APPID=" +  appId;
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => this.setState({weatherData:data}))
+      .catch(error => console.error(error));
+
+    event.preventDefault();
+  }
+
   render(){
     const cloud = "/img/cloud.png";
     const sun = "/img/sun.png";
+    if (this.state.weatherData.length === 0) {
+      return(
+        <span> Loading... </span>
+      );
+    }
     return(
       <div className="app_wrapper">
         <div className="app">
@@ -62,6 +88,12 @@ class App extends Component {
             {this.renderCard(2, this.state.forecastDays[2], sun)}
             {this.renderCard(3, this.state.forecastDays[3], sun)}
             {this.renderCard(4, this.state.forecastDays[4], cloud)}
+        </div>
+        <div className="changeForm">
+          <form onSubmit={this.handleSubmit}>
+            City: <input type="text" value={this.state.city} onChange={this.handleCityChange}/>
+                  <input type="submit" value="Submit"/>
+          </form>
         </div>
       </div>
     );
--- src/TitleBar.js
+++ src/TitleBar.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function TitleBar(props) {
-    return(
-      <div className="title_bar">
-        <div className="tb_float_left">
-          <p>{props.city}</p>
+class TitleBar extends Component {
+    constructor(props){
+      super(props);
+      var printMinutes = this.props.minutes;
+      if (this.props.minutes < 10){
+        printMinutes = "0" + this.props.minutes;
+      }
+      this.state = {
+        minutes: printMinutes,
+      };
+
+    }
+    render(){
+      return(
+        <div className="title_bar">
+          <div className="tb_float_left">
+            <p>{this.props.city}</p>
+          </div>
+          <div className="tb_float_right">
+            <p>{this.props.hours}:{this.state.minutes}</p>
+          </div>
+          <div className="clearBoth"></div>
         </div>
-        <div className="tb_float_right">
-          <p>{props.hours}:{props.minutes}</p>
-        </div>
-        <div className="clearBoth"></div>
-      </div>
-    );
+      );
+    }
 }
 
 export default TitleBar;
